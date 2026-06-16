d0df4eb18b788b79b10b87925011b9dd46e4bc78
Bidasep <tornado16@hotmail.com>
2026-06-16 18:42:48 -0300

    Feature/RedisBasic

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 #inicialização do redis obs. configuração para rodar localmente
-redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 
 
 app = FastAPI()
